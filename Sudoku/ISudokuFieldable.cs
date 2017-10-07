d045cad12250794a216cea77e0f4f1f9b3b96d93
bramble100 <arnold.barna@gmail.com>
2017-10-07 12:25:47 +0200

    feat(ISudokuFieldable): create interface

---
+++ Sudoku/ISudokuFieldable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Methods for the cells and groups of cells in the sudoku.
+    /// </summary>
+    interface ISudokuFieldable
+    {
+        /// <summary>
+        /// Returns true if cell(s) contain(s) the given number.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        bool Contains(int number);
+
+        /// <summary>
+        /// Returns an IEnumerable<int> of the available numbers.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<int> AvailableNumbers();
+
+        /// <summary>
+        /// Returns true if given number can be placed in the cell(s).
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        bool NumberIsPlaceable(int number);
+    }
+}
