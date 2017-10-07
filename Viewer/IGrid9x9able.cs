3473f3e8b9f3464eabe5aeeecc52b4559635e9e9
bramble100 <arnold.barna@gmail.com>
2017-10-07 21:50:37 +0200

    feat(Viewer): add interfaces

---
+++ Viewer/IGrid9x9able.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuModel;
+
+namespace Viewer
+{
+    interface IGrid9x9able
+    {
+        void Initialize(int cellSize);
+
+        void DrawGrid();
+
+        void PlaceNumber(IPositionable position, int number);
+
+        void RemoveNumber(IPositionable position);
+
+        void HighlightCell(IPositionable position);
+
+
+    }
+}
