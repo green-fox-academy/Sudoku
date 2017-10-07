868ee430f01dd8446329169b0c21f5af98efe9e5
bramble100 <arnold.barna@gmail.com>
2017-10-07 12:31:30 +0200

    feat(ISudokuCellable): create interface

---
+++ Sudoku/ISudokuCellable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Methods, properties for a cell in the sudoku.
+    /// </summary>
+    interface ISudokuCellable
+    {
+        /// <summary>
+        /// Represents the number that can be found in the cell.
+        /// </summary>
+        int Value { get; set; }
+    }
+}
