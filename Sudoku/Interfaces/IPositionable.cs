7b02940fbd0376901bdd6237b465d5adc0cc695c
bramble100 <arnold.barna@gmail.com>
2017-10-07 21:51:16 +0200

    feat(Model): minor cahnges in interfaces

---
+++ Sudoku/Interfaces/IPositionable.cs
@@ -0,0 +1,18 @@
+﻿namespace SudokuModel
+{
+    /// <summary>
+    /// Interface for holding the position with X and Y coordinates.
+    /// </summary>
+    public interface IPositionable
+    {
+        /// <summary>
+        /// The X coordinate of the position.
+        /// </summary>
+        int X { get; set; }
+
+        /// <summary>
+        /// The X coordinate of the position.
+        /// </summary>
+        int Y { get; set; }
+    }
+}
--- Sudoku/Interfaces/ISudokuFieldable.cs
+++ Sudoku/Interfaces/ISudokuFieldable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sudoku
+namespace SudokuModel
 {
     /// <summary>
     /// Methods for the cells and groups of cells in the sudoku.
