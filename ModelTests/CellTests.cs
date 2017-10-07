d324b8b5acefd3807cb89ab6ba85931dfc8e8d84
bramble100 <arnold.barna@gmail.com>
2017-10-07 21:07:58 +0200

    test (Model): add test for Cell class

---
+++ ModelTests/CellTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sudoku;
+
+namespace ModelTests
+{
+    [TestFixture]
+    public class CellTests
+    {
+        [Test]
+        public void WhenCellIsEmpty()
+        {
+        }
+    }
+}
